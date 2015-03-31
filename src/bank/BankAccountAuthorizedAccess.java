4c5098c566aa3e1324b602ac8f912cef1c726f0f
Makemek <makemek101@hotmail.com>
2015-03-31 16:30:58 +0700

    create new interface to distinguish access level

---
+++ src/bank/BankAccountAuthorizedAccess.java
@@ -0,0 +1,10 @@
+package bank;
+
+/**
+ * Supplement interface
+ * Created by Apipol on 31/03/15.
+ */
+public interface BankAccountAuthorizedAccess extends BankAccountPublicAccess{
+    public void deposit(int amt);
+    public void withdraw(int amt);
+}
